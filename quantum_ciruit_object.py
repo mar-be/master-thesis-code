e8fc5582cefe93b4b973524c3e34134314136b41
Marvin Bechtold <marvin.bechtold@gmail.com>
2020-12-15 09:47:04 +0100

    qc_modification

--- quantum_ciruit_object.py
+++ quantum_ciruit_object.py
@@ -1,14 +1,25 @@
+from typing import List
 from qiskit import QuantumCircuit
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.schema import ForeignKey
 import sqlalchemy.types as types
 from sqlalchemy import create_engine, Column
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import backref, relationship, sessionmaker
 
 
 # declarative base class
 Base = declarative_base()
 engine = create_engine('sqlite:///foo.db')
 session = sessionmaker(engine)()
+
+class Modification(Base):
+
+    __tablename__ = "qc_modification"
+    id = Column(types.Integer, primary_key=True)
+    type = Column(types.String, nullable=False)
+
+    input_circuit = relationship("QC_Object", backref="modification_input", foreign_keys="QC_Object.input_modification_id")
+    output_circuit = relationship("QC_Object", backref="modification_output", foreign_keys="QC_Object.output_modification_id")
 
 
 class QC_Object(Base):
@@ -17,6 +28,9 @@ class QC_Object(Base):
 
     id = Column(types.Integer, primary_key=True)
     _qasm = Column("qasm", types.String, nullable=False)
+    input_modification_id = Column(types.Integer, ForeignKey("qc_modification.id"))
+    output_modification_id = Column(types.Integer, ForeignKey("qc_modification.id"))
+    
 
     def __init__(self,  circuit:QuantumCircuit) -> None:
         self.circuit = circuit
@@ -39,11 +53,12 @@ class QC_Object(Base):
         return cls(circuit)
 
 
+    
 
-        
+    
 
 if __name__ == "__main__":
-    QC_Object.metadata.create_all(engine)
+    Base.metadata.create_all(engine)
 
     # Create a Quantum Circuit acting on the q register
     circuit = QuantumCircuit(2, 2)
@@ -59,8 +74,28 @@ if __name__ == "__main__":
 
     qc_obj = QC_Object.from_qasm(circuit.qasm())
 
+    # Create a Quantum Circuit acting on the q register
+    circuit_2 = QuantumCircuit(2, 2)
+
+    # Add a H gate on qubit 0
+    circuit_2.h(0)
+
+    # Map the quantum measurement to the classical bits
+    circuit_2.measure([0,1], [0,1])
+
+
+    qc_obj_2 = QC_Object(circuit_2)
+    mod = Modification()
+    mod.input_circuit.append(qc_obj)
+    mod.input_circuit.append(qc_obj_2)
+    mod.output_circuit.append(qc_obj_2)
+    mod.type = "test"
+
     session.add(qc_obj)
+    session.add(qc_obj_2)
+
     session.commit()
+
 
     print(qc_obj.id)
     print(qc_obj.circuit.qasm())
