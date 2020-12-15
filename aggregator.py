4532c9753700d024fbc4a76b4ee7420e9cb59dbf
Marvin Bechtold <marvin.bechtold@gmail.com>
2020-12-15 10:23:09 +0100

    aggregate_qc_obj

--- aggregator.py
+++ aggregator.py
@@ -1,4 +1,5 @@
 from qiskit import QuantumCircuit
+from quantum_ciruit_object import QC_Object, Modification, session
 
 def aggregate(list_of_circuits: list[QuantumCircuit]) -> QuantumCircuit:
     num_qubits = 0
@@ -20,3 +21,47 @@ def aggregate(list_of_circuits: list[QuantumCircuit]) -> QuantumCircuit:
         clbit_count += len(circ.clbits)
 
     return agg_circuit
+
+def aggregate_qc_obj(list_of_circuits: list[QC_Object]) -> QC_Object:
+    agg_circuit = aggregate([qc_obj.circuit for qc_obj in list_of_circuits])
+    agg_qc_obj = QC_Object(agg_circuit)
+    agg_mod = Modification()
+    agg_mod.input_circuit.extend(list_of_circuits)
+    agg_mod.output_circuit.append(agg_qc_obj)
+    agg_mod.type = "aggregation"
+    session.add(agg_mod)
+    session.commit()
+    return agg_qc_obj
+
+
+if __name__ == "__main__":
+    # Create a Quantum Circuit acting on the q register
+    circuit = QuantumCircuit(2, 2)
+
+    # Add a H gate on qubit 0
+    circuit.h(0)
+
+    # Add a CX (CNOT) gate on control qubit 0 and target qubit 1
+    circuit.cx(0, 1)
+
+    # Map the quantum measurement to the classical bits
+    circuit.measure([0,1], [0,1])
+
+    qc_obj = QC_Object.from_qasm(circuit.qasm())
+
+    # Create a Quantum Circuit acting on the q register
+    circuit_2 = QuantumCircuit(2, 2)
+
+    # Add a H gate on qubit 0
+    circuit_2.h(0)
+
+    # Map the quantum measurement to the classical bits
+    circuit_2.measure([0,1], [0,1])
+
+
+    qc_obj_2 = QC_Object(circuit_2)
+
+    agg = aggregate_qc_obj([qc_obj, qc_obj_2])
+
+    print(len(agg.modification_output.input_circuit))
+
