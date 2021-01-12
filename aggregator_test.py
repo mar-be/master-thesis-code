a08d5f27470e08db4d3623d6ac3864c01d909bc5
Marvin Bechtold <marvin.bechtold@gmail.com>
2021-01-12 15:45:35 +0100

    Refactoring

--- aggregator_test.py
+++ aggregator_test.py
@@ -50,31 +50,31 @@ if __name__ == "__main__":
     backend_state = Aer.get_backend('statevector_simulator')
 
 
-    circ_a = gen_BV('a')
-    circ_ab = gen_BV('ab')
+    # circ_1 = gen_BV('a')
+    circ_2 = gen_BV('ab')
 
-    circuit = QuantumCircuit(2, 2)
-    circuit.h(0)
-    circuit.h(1)
-
-
-    job_a_state = execute(circuit, backend_state)
-    job_ab_state = execute(circ_ab, backend_state)
+    circ_1 = QuantumCircuit(2, 2)
+    circ_1.h(0)
+    circ_1.h(1)
 
 
-    circ_a.measure_all()
-    circ_ab.measure_all()
-    circuit.measure([0,1], [0,1])
+    job_1_state = execute(circ_1, backend_state)
+    job_2_state = execute(circ_2, backend_state)
+
+
+    # circ_1.measure_all()
+    circ_2.measure_all()
+    circ_1.measure([0,1], [0,1])
 
     #q_job = Quantum_Job(circ_a)
-    q_job = Quantum_Job(circuit)
-    q_job2 = Quantum_Job(circ_ab)
+    q_job_1 = Quantum_Job(circ_1)
+    q_job_2 = Quantum_Job(circ_2)
 
-    session.add(q_job)
-    session.add(q_job2)
+    session.add(q_job_1)
+    session.add(q_job_2)
     session.commit()
 
-    agg_job = aggregate_q_jobs([q_job, q_job2])
+    agg_job = aggregate_q_jobs([q_job_1, q_job_2])
 
     agg_circuit = agg_job.circuit
 
@@ -82,39 +82,39 @@ if __name__ == "__main__":
 
 
     
-    job_a = execute(circuit, backend, shots=8192)
-    job_ab = execute(circ_ab, backend, shots=8192)
+    job_1 = execute(circ_1, backend, shots=8192)
+    job_2 = execute(circ_2, backend, shots=8192)
     job_agg = execute(agg_circuit, backend, shots=8192)
 
     agg_job.qiskit_job_id = job_agg.job_id()
 
     res_agg = split_results(job_agg.result(), agg_job)
 
-    res_a_agg = res_agg[0]
-    res_ab_agg = res_agg[1]
+    res_1_agg = res_agg[0]
+    res_2_agg = res_agg[1]
 
-    res_a = job_a.result()
-    res_ab = job_ab.result()
+    res_1 = job_1.result()
+    res_2 = job_2.result()
 
-    res_a_state = job_a_state.result()
-    res_ab_state = job_ab_state.result()
+    res_1_state = job_1_state.result()
+    res_2_state = job_2_state.result()
 
     print("\n" + 20*"*" + "Counts" + 20*"*")
-    print(f"a count:{res_a.get_counts()}")
-    print(f"a_agg count:{res_a_agg.get_counts()}")
-    print(f"a_state count:{res_a_state.get_counts()}")
-    print(f"ab count:{res_ab.get_counts()}")
-    print(f"ab_agg count:{res_ab_agg.get_counts()}")
-    print(f"ab_state count:{res_ab_state.get_counts()}")
+    print(f"circ_1 count:{res_1.get_counts()}")
+    print(f"circ_1_agg count:{res_1_agg.get_counts()}")
+    print(f"circ_1_state count:{res_1_state.get_counts()}")
+    print(f"circ_2 count:{res_2.get_counts()}")
+    print(f"circ_2_agg count:{res_2_agg.get_counts()}")
+    print(f"circ_2_state count:{res_2_state.get_counts()}")
     print(f"count:{job_agg.result().get_counts()}")
 
     print("\n" + 20*"*" + "Chi^2" + 20*"*")
-    chi_2_a = calc_chi_2(res_a, res_a_state, True)
-    chi_2_a_agg = calc_chi_2(res_a_agg, res_a_state, True)
-    chi_2_ab = calc_chi_2(res_ab, res_ab_state, True)
-    chi_2_ab_agg = calc_chi_2(res_ab_agg, res_ab_state, True)
-    print(f"chi^2 of a: {chi_2_a}")
-    print(f"chi^2 of a_agg: {chi_2_a_agg}")
-    print(f"chi^2 of ab: {chi_2_ab}")
-    print(f"chi^2 of ab_agg: {chi_2_ab_agg}")
+    chi_2_1 = calc_chi_2(res_1, res_1_state, True)
+    chi_2_1_agg = calc_chi_2(res_1_agg, res_1_state, True)
+    chi_2_2 = calc_chi_2(res_2, res_2_state, True)
+    chi_2_2_agg = calc_chi_2(res_2_agg, res_2_state, True)
+    print(f"chi^2 of circ_1: {chi_2_1}")
+    print(f"chi^2 of circ_1_agg: {chi_2_1_agg}")
+    print(f"chi^2 of circ_2: {chi_2_2}")
+    print(f"chi^2 of circ_2_agg: {chi_2_2_agg}")
 
