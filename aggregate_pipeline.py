ebb4a795da27a9090b8bf1a2194c6b26e2e236c4
Marvin Bechtold <marvin.bechtold@gmail.com>
2021-02-19 18:30:02 +0100

    adapted aggregate_pipeline

--- aggregate_pipeline.py
+++ aggregate_pipeline.py
@@ -1,3 +1,4 @@
+from logger import get_logger
 from queue import Queue
 from execution_handler.execution_handler import ExecutionHandler
 from aggregator.aggregator import Aggregator, AggregatorResults
@@ -6,6 +7,7 @@ from quantum_job import QuantumJob
 from qiskit.circuit.random import random_circuit
 
 if __name__ == "__main__":
+    log = get_logger("Evaluate")
     input = Queue()
     output_agg = Queue()
     output_exec = Queue()
@@ -22,17 +24,23 @@ if __name__ == "__main__":
     aggregator = Aggregator(input, output_agg, agg_job_dict, 10)
     aggregator.start()
 
-    exec_handler = ExecutionHandler(backend_sim, output_agg, output_exec)
+    exec_handler = ExecutionHandler(provider, output_agg, output_exec, batch_timeout=10)
+    exec_handler.start()
 
     aggregator_results = AggregatorResults(output_exec, output, agg_job_dict)
     aggregator_results.start()
 
-    for i in range(900):
-        input.put(QuantumJob(random_circuit(5, 5, measure=True), shots=10000))
+    for i in range(200):
+        if i % 2 == 0:
+            backend = 'ibmq_qasm_simulator'
+        else:
+            backend = 'ibmq_quito'
+        # backend = 'ibmq_qasm_simulator'
+        input.put(QuantumJob(random_circuit(2, 5, measure=True), shots=10000, backend=backend))
     
     i = 0
     while True:
         job = output.get()
         r = job.result
-        print(i, job.id, r.success)
+        log.info(f"{i}: Got job {job.id},type {job.type}, from backend {job.backend}, success: {r.success}")
         i += 1
