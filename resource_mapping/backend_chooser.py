70fc0ac7d166556c4d714ef710f3e684ad5c7502
Marvin Bechtold <marvin.bechtold@gmail.com>
2021-05-12 17:00:03 +0200

    comments and formating

--- resource_mapping/backend_chooser.py
+++ resource_mapping/backend_chooser.py
@@ -1,17 +1,16 @@
 import copy
 import time
-from typing import Any, Dict, List, Callable, Tuple, Optional, Union
-from qiskit import IBMQ, QuantumCircuit
-from qiskit.providers.ibmq import least_busy
-from qiskit import IBMQ
-from qiskit.providers import Provider, Backend
-from qiskit.providers.ibmq.ibmqbackend import IBMQBackend
+from typing import Callable, Dict, List, Optional, Tuple, Union
 
+from qiskit.providers import Backend, Provider
 
 
 class Backend_Data():
+    """
+    Caches the data locally for a remote backend
+    """
 
-    def __init__(self, backend:Backend) -> None:
+    def __init__(self, backend: Backend) -> None:
         self.name = backend.name()
         self.n_qubits = backend.configuration().n_qubits
         self.operational = backend.status().operational
@@ -31,8 +30,12 @@ class Backend_Data():
 
 
 class Backend_Chooser():
+    """
+    Allows to query the local knowledgebase about the remote backends.
+    It updates periodically the cached inforamtion.
+    """
 
-    def __init__(self, provider: Provider, config:Dict, update_interval: int = 60) -> None:
+    def __init__(self, provider: Provider, config: Dict, update_interval: int = 60) -> None:
         self._provider = provider
         self._config = config
         self._update_interval = update_interval
@@ -40,9 +43,19 @@ class Backend_Chooser():
         self._last_update = time.time()
         self._update_backends()
 
-    def _backend_filter(self, backend:Union[Backend_Data,Tuple[str, Backend_Data]], filter_dict:Dict=None, filter_func:Callable[[Backend_Data],bool]=None) -> bool:
+    def _backend_filter(self, backend: Union[Backend_Data, Tuple[str, Backend_Data]], filter_dict: Dict = None, filter_func: Callable[[Backend_Data], bool] = None) -> bool:
+        """Applies the filter_dict and the filter_func to the backend and evaluates it
+
+        Args:
+            backend (Union[Backend_Data,Tuple[str, Backend_Data]])
+            filter_dict (Dict, optional): Dict that filters the backend. It has the same format as the backend_chooser part in the config file. If it is None, no filter is applied. Defaults to None.
+            filter_func (Callable[[Backend_Data],bool], optional): Filter the backends via a callable that gets as input a Backend_Data object and outputs a bool. If it is None, no filter is applied. Defaults to None.
+
+        Returns:
+            bool: Return True, if the backend is compliant with the filters
+        """
         if isinstance(backend, Tuple):
-            _ , backend = backend
+            _, backend = backend
         if filter_dict is None:
             # use default config
             config = self._config
@@ -51,7 +64,7 @@ class Backend_Chooser():
             config = copy.deepcopy(self._config)
             config.update(filter_dict)
         if filter_func is None:
-            filter_func = lambda x:True
+            def filter_func(x): return True
 
         if backend.name in config["backend_black_list"]:
             return False
@@ -69,50 +82,73 @@ class Backend_Chooser():
             if not config["quantum_volume"]["max"] is None and config["quantum_volume"]["max"] < backend.quantum_volume:
                 return False
         return filter_func(backend)
-        
 
     def _update_backends(self):
+        """
+        Cache all information about the remote backends
+        """
         for b in self._provider.backends():
             self._backends[b.name()] = Backend_Data(b)
 
     def _check_update_backends(self):
+        """Check if the local knowledge base about the remote backends needs an update
+        """
         now = time.time()
         if self._last_update + self._update_interval < now:
             self._update_backends()
             self._last_update = now
 
-    def get_backends(self, filter_dict:Dict=None, filter_func:Optional[Callable[[Backend_Data],bool]] = None) -> Dict:
+    def get_backends(self, filter_dict: Dict = None, filter_func: Optional[Callable[[Backend_Data], bool]] = None) -> Dict:
+        """Query the local knowledge base about the remote backends
+
+        Args:
+            filter_dict (Dict, optional): Dict that filters the backends. It has the same format as the backend_chooser part in the config file. If it is None, no filter is applied. Defaults to None.
+            filter_func (Optional[Callable[[Backend_Data],bool]], optional): Filter the backends via a callable that gets as input a Backend_Data object and outputs a bool. If it is None, no filter is applied. Defaults to None.
+
+        Returns:
+            Dict: key is the name of the backend, value is a Backend_Data object
+        """
         self._check_update_backends()
         if filter_func == None:
             return self._backends
-        backend_filter = lambda b: self._backend_filter(b, filter_dict, filter_func)
+
+        def backend_filter(b): return self._backend_filter(
+            b, filter_dict, filter_func)
         return dict(filter(backend_filter, self._backends.items()))
-    
-    def get_backend_names(self, filter_dict:Dict=None, filter_func:Optional[Callable[[Backend_Data],bool]] = None) -> List[str]:
+
+    def get_backend_names(self, filter_dict: Dict = None, filter_func: Optional[Callable[[Backend_Data], bool]] = None) -> List[str]:
+        """Query the local knowledge base about the remote backends
+
+        Args:
+            filter_dict (Dict, optional): Dict that filters the backends. It has the same format as the backend_chooser part in the config file. If it is None, no filter is applied. Defaults to None.
+            filter_func (Optional[Callable[[Backend_Data],bool]], optional): Filter the backends via a allable that gets as input a Backend_Data object and outputs a bool. If it is None, no filter is applied. Defaults to None.
+
+        Returns:
+            List[str]: list of backend names
+        """
         return self.get_backends(filter_dict, filter_func).keys()
 
-    def get_least_busy(self, filter_dict:Dict=None, filter_func:Optional[Callable[[Backend_Data],bool]] = None) -> Optional[Tuple[str, Dict]]:
+    def get_least_busy(self, filter_dict: Dict = None, filter_func: Optional[Callable[[Backend_Data], bool]] = None) -> Optional[Tuple[str, Dict]]:
+        """Return the least busy remote backend from the local knowledge base 
+
+        Args:
+            filter_dict (Dict, optional): Dict that filters the backends. It has the same format as the backend_chooser part in the config file. If it is None, no filter is applied. Defaults to None.
+            filter_func (Optional[Callable[[Backend_Data],bool]], optional): Filter the backends via a callable that gets as input a Backend_Data object and outputs a bool. If it is None, no filter is applied. Defaults to None.
+
+        Returns:
+            Optional[Tuple[str, Dict]]: Tuple containing the backend name and a Backend_Data object. Return None if no backend is available.
+        """
         if filter_func is None:
-            f = lambda x: x.operational and x.status_msg == "active"
+            def f(x): return x.operational and x.status_msg == "active"
         else:
-            f = lambda x: filter_func(x) and x.operational and x.status_msg == "active"
-        filtered_backends = self.get_backends(filter_dict=filter_dict, filter_func=f)
+            def f(x): return filter_func(
+                x) and x.operational and x.status_msg == "active"
+        filtered_backends = self.get_backends(
+            filter_dict=filter_dict, filter_func=f)
         if len(filtered_backends) == 0:
             return None
-        backends_with_free_jobs = dict(filter(lambda backend: backend[1].active_jobs < backend[1].maximum_jobs, filtered_backends.items()))
-        least_busy = min(backends_with_free_jobs.items(), key=lambda x: x[1].pending_jobs)
-        return least_busy 
-        
-
-
-
-if __name__ == "__main__":
-    IBMQ.load_account()
-    provider = IBMQ.get_provider()
-    bc =  Backend_Chooser(provider)
-    backend_name, backend_data = bc.get_least_busy(filters=lambda x: x.n_qubits>=5 and not x.simulator and x.quantum_volume >= 16)
-    print(backend_name)
-    print(backend_data)
-    # print([b.name() for b in sb])
-    # print([b.status().to_dict() for b in sb])
-    # print(least_busy(sb))
+        backends_with_free_jobs = dict(filter(
+            lambda backend: backend[1].active_jobs < backend[1].maximum_jobs, filtered_backends.items()))
+        least_busy = min(backends_with_free_jobs.items(),
+                         key=lambda x: x[1].pending_jobs)
+        return least_busy
--- virtualization.py
+++ virtualization.py
@@ -1,21 +1,29 @@
-from typing import Dict
-from partitioner.partition_result_processing import ResultProcessing, ResultWriter
-from resource_mapping.result_analyzer import ResultAnalyzer
-from execution_handler.execution_handler import ExecutionHandler
-from partitioner.partitioner import Partitioner
-from aggregator.aggregator import Aggregator, AggregatorResults
-from resource_mapping.quantum_resource_mapper import QuantumResourceMapper
-from resource_mapping.backend_chooser import Backend_Chooser
 from queue import Queue
+from typing import Dict
 
 from qiskit.providers.provider import Provider
+
 import logger
+from aggregator.aggregator import Aggregator, AggregatorResults
+from execution_handler.execution_handler import ExecutionHandler
+from partitioner.partition_result_processing import (ResultProcessing,
+                                                     ResultWriter)
+from partitioner.partitioner import Partitioner
+from resource_mapping.backend_chooser import Backend_Chooser
+from resource_mapping.quantum_resource_mapper import QuantumResourceMapper
+from resource_mapping.result_analyzer import ResultAnalyzer
+
 
 class Virtual_Execution_Environment():
 
-    def __init__(self, provider: Provider, config:Dict) -> None:
+    def __init__(self, provider: Provider, config: Dict) -> None:
+        """
+        Creates a Virtual_Execution_Environment object
+        Args:
+            provider (Provider): provider object that handles the communication with IBMQ
+            config (Dict): configuration
+        """
         self._log = logger.get_logger(type(self).__name__)
-
 
         self.input = Queue()
         self.output = Queue()
@@ -31,23 +39,25 @@ class Virtual_Execution_Environment():
         input_partition_result = Queue()
         all_results_are_available = Queue()
 
-
-
         aggregation_dict = {}
         partition_dict = {}
 
         self.backend_chooser = Backend_Chooser(provider, config["quantum_resource_mapper"]["backend_chooser"])
-        self.quantum_resource_mapper = QuantumResourceMapper(input=self.input, output=input_execution, output_agg=input_aggregation, output_part=input_partition, backend_chooser=self.backend_chooser, config=config["quantum_resource_mapper"])
-        self.aggregator = Aggregator(input=input_aggregation, output=input_execution, job_dict=aggregation_dict, timeout=config["aggregator"]["timeout"])
-        self.partitioner = Partitioner(input=input_partition, output=input_execution, partition_dict=partition_dict, error_queue=self.errors, **config["partitioner"])
+        self.quantum_resource_mapper = QuantumResourceMapper(input=self.input, output=input_execution, output_agg=input_aggregation,
+                                                             output_part=input_partition, backend_chooser=self.backend_chooser, config=config["quantum_resource_mapper"])
+        self.aggregator = Aggregator(input=input_aggregation, output=input_execution,
+                                     job_dict=aggregation_dict, timeout=config["aggregator"]["timeout"])
+        self.partitioner = Partitioner(input=input_partition, output=input_execution,
+                                       partition_dict=partition_dict, error_queue=self.errors, **config["partitioner"])
         self.execution_handler = ExecutionHandler(provider, input=input_execution, output=output_execution, **config["execution_handler"])
         self.result_analyzer = ResultAnalyzer(input=output_execution, output=self.output, output_agg=input_aggregation_result, output_part=input_partition_result)
         self.aggregation_result_processor = AggregatorResults(input=input_aggregation_result, output=self.output, job_dict=aggregation_dict)
         self.partition_result_writer = ResultWriter(input=input_partition_result, completed_jobs=all_results_are_available, partition_dict=partition_dict)
         self.partition_result_processor = ResultProcessing(input=all_results_are_available, output=self.output, partition_dict=partition_dict)
 
-    
     def start(self):
+        """Start all threads of the Virtual_Execution_Environment object
+        """
         self.quantum_resource_mapper.start()
         self.aggregator.start()
         self.partitioner.start()
